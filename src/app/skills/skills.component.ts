70a1100a45504e026b69936083f465047f990f15
Tom <tomlazore@gmail.com>
2025-06-13 16:58:16 -0400

    Updated skills section to have a marquee of icons. Made some CSS changes for better UI

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
 
+  techs = [
+    { name: 'Angular',      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg' },
+    { name: 'React',        src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
+    { name: 'Node.js',      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg' },
+    { name: 'NestJS',       src: 'https://raw.githubusercontent.com/devicons/devicon/6910f0503efdd315c8f9b858234310c06e04d9c0/icons/nestjs/nestjs-original.svg' },
+    { name: 'Express',      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg' },
+    { name: 'Tailwind CSS', src: 'https://cdn.simpleicons.org/Tailwindcss' },
+    { name: 'Python',       src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
+    { name: 'Java',         src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' },
+    { name: 'Spring',       src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg' },
+    { name: 'PWA',          src: 'https://images.seeklogo.com/logo-png/36/2/progressive-web-apps-logo-png_seeklogo-368489.png' },
+    { name: 'AWS',          src: 'https://raw.githubusercontent.com/devicons/devicon/refs/tags/v2.16.0/icons/amazonwebservices/amazonwebservices-original-wordmark.svg' },
+    { name: 'Azure',        src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg' },
+    { name: 'Postgres',     src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' },
+    { name: 'MongoDB',      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' },
+    { name: 'OpenAI',       src: 'https://cdn.simpleicons.org/OpenAI' },
+    { name: 'Docker',       src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg' },
+  ];
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode:'class',
-  content: ["./src/**/*.{html,ts}"],
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,ts}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        // creates a class `animate-spin-slow`
+        'spin-slow': 'spin 20s linear infinite',
+      },
+    }
+  },
+  variants: {
+    extend: {
+      animation: ['hover'],   // allow `hover:animate-none` later
+    }
   },
   plugins: [],
 }
-
