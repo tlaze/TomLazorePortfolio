0f825cece354cf8c626dbee6bbb0de034af76a48
Tom <tomlazore@gmail.com>
2025-06-13 15:25:30 -0400

    added dark mode

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isDark = false;
 
+  constructor(private renderer: Renderer2) {}
+
+  toggleDarkMode() {
+    this.isDark = !this.isDark;
+    localStorage.setItem('theme', this.isDark ? 'dark' : 'light');
+    this.updateBodyClass();
+  }
+
+  ngOnInit() {
+    this.isDark = localStorage.getItem('theme') === 'dark';
+    this.updateBodyClass();
+  }
+
+  private updateBodyClass() {
+    if (this.isDark) {
+      this.renderer.addClass(document.body, 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark');
+    }
+  }
 }
