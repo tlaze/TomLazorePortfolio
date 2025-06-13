01f015383c8923aa1840b2a8782e5df01f02c408
Tom <tomlazore@gmail.com>
2025-06-13 13:48:26 -0400

    updatd skills section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
     NavbarComponent,
     AboutComponent,
+    SkillsComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
